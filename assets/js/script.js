9a34bfec7b3b23126b20460ba4604442cfed5d5f
iweinacker <>
2023-05-30 17:26:08 +0000

    add function to disable edit & delete button and add function to modify list elements

--- assets/js/script.js
+++ assets/js/script.js
@@ -26,8 +26,31 @@ totalAmountButton.addEventListener("click", () => {
         amount.innerHTML = tempAmount - expenditureValue.innerText;
         //Clear Input Box
         totalAmount.value="";
-
     }
 });
 
+//Function To Disable Edit and Delete Button
 
+const disableButtons = (bool) => {
+    let editButtons = dpcument.getElementsByClassName("edit");
+    Array.from(editButtons).forEach(element => {
+        element.disabled = bool;
+    })
+};
+
+//Function to Modify List Elements
+const modifyElement = (element, edit = false) => {
+    let parentDiv = element.parentElement;
+    let currentBalance = balanceValue.innerText;
+    let currentExpense = expenditureValue.innerText;
+    let parentAmount = parentDiv.querySelector(".amount").innerText;
+    if (edit) {
+        let parentText = parentDiv.querySelector(".product").innerText;
+        productTitle.value = parentText;
+        userAmount.value = parentAmount;
+        disableButtons(true);
+    }
+    balanceValue.innerText = parseInt(currentBalance) = parseInt(parentAmount);
+    expenditureValue.innerText = parseInt(currentExpense) - parseInt(parentAmount);
+    parentDiv.remove();
+};
